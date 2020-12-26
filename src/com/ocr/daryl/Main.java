2d46b233b77829f90a12e63e0bf2cfd7f34bb611
Daryl Livio <daryl.livio@gmail.com>
2020-12-26 20:37:24 +0100

    First commit exemple

---
+++ src/com/ocr/daryl/Main.java
@@ -0,0 +1,15 @@
+package com.ocr.daryl;
+
+/**
+* Cette classe est la classe principal pour le lancement de l'application"
+* @author Daryl LIVIO
+*/
+
+public class Main {
+    public static void main(String[] args) {
+        
+        System.out.println("Salut la miff");
+        
+    }
+
+}
